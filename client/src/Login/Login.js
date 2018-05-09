1443428bc833ede6c488a73da13b0e8bbfc6acf7
Ahmed Zakir <ahmadmzak@gmail.com>
2018-05-09 18:21:10 +0300

    added junior as default user

--- client/src/Login/Login.js
+++ client/src/Login/Login.js
@@ -1,35 +1,35 @@
 import React, { Component } from "react";
 
 class Login extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      value: ""
+    constructor(props) {
+        super(props);
+        this.state = {
+            value: "junior" //""
+        };
+    }
+    handleClick = event => {
+        this.props.handleLogin(this.state.value);
     };
-  }
-  handleClick = event => {
-    this.props.handleLogin(this.state.value);
-  };
-  handleChange = event => {
-    this.setState({ value: event.target.value });
-  };
-  render() {
-    return (
-      <div className="container">
-        <input
-          type="text"
-          placeholder="username"
-          className="login"
-          value={this.state.value}
-          onChange={this.handleChange}
-        />
-        <br />
-        <button className="login" onClick={this.handleClick}>
-          login
-        </button>
-      </div>
-    );
-  }
+    handleChange = event => {
+        this.setState({ value: event.target.value });
+    };
+    render() {
+        return (
+            <div className="container">
+                <input
+                    type="text"
+                    placeholder="username"
+                    className="login"
+                    value={this.state.value}
+                    onChange={this.handleChange}
+                />
+                <br />
+                <button className="login" onClick={this.handleClick}>
+                    login
+                </button>
+            </div>
+        );
+    }
 }
 
 export default Login;
